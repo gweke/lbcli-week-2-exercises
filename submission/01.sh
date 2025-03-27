f8e98b33ec6072f33f5feb0273f0543d7a0aae87
William EKE <gwilliam.eke@gmail.com>
2025-03-27 06:45:25 +0100

    deleting the loadwallet command from 01.sh and 02.sh that causes errors

--- submission/01.sh
+++ submission/01.sh
@@ -4,7 +4,7 @@ echo $(bitcoin-cli -regtest createwallet $wallet_name)
 
 #echo "Load the wallet : $wallet_name"
 #echo "Load the wallet : $wallet_name"
-load_wallet=$(bitcoin-cli -regtest loadwallet $wallet_name)
+#load_wallet=$(bitcoin-cli -regtest loadwallet $wallet_name)
 
 #echo "Get the wallet info : $wallet_name"
 #echo $(bitcoin-cli -regtest -rpcwallet=$wallet_name getwalletinfo)
--- submission/02.sh
+++ submission/02.sh
@@ -2,7 +2,7 @@
 wallet_name="btrustwallet"
 
 #echo "Load the wallet : $wallet_name"
-load_wallet=$(bitcoin-cli -regtest loadwallet $wallet_name)
+#load_wallet=$(bitcoin-cli -regtest loadwallet $wallet_name)
 
 # we use getrawchangeaddress that will generate bech32 address type
 change_address=$(bitcoin-cli -regtest -rpcwallet=$wallet_name getrawchangeaddress)
